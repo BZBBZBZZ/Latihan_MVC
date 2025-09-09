f565956a1528f92a974ecf20273444852e64036c
Nicho <nicholasleroy83@gmail.com>
2025-09-09 18:40:17 +0700

    fix

--- view/view_addmahasiswa.php
+++ view/view_addmahasiswa.php
@@ -2,49 +2,46 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
-        integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+    integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+  <title>Document</title>
 </head>
 
 <body>
-    <div class="container p-3">
-      <div class="card text-center">
-  <div class="card-header">
-    <ul class="nav nav-tabs card-header-tabs">
-      <li class="nav-item">
-        <a class="nav-link active" href="view_mahasiswa.php">Mahasiswa</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link active" href="view_addmahasiswa.php">New Mahasiswa</a>
-      </li>
-    </ul>
-  </div>
-  <div class="card-body">
-    <h1>New Mahasiswa</h1>
+  <div class="container p-3">
+    <div class="card text-center">
+      <div class="card-header">
+        <ul class="nav nav-tabs card-header-tabs">
+          <li class="nav-item">
+            <a class="nav-link active" href="view_mahasiswa.php">Mahasiswa</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="view_addmahasiswa.php">New Mahasiswa</a>
+          </li>
+        </ul>
+      </div>
+      <div class="card-body">
+        <h1>New Mahasiswa</h1>
         <form>
-  <div class="form-row">
-  </div>
-  <div class="form-group">
-    <label for="inputNama">Nama</label>
-    <input type="text" class="form-control" id="inputNama" placeholder="Masukkan Nama">
-  </div>
-  <div class="form-group">
-    <label for="inputUsia">Usia</label>
-    <input type="text" class="form-control" id="inputUsia" placeholder="Masukkan Usia">
-  </div>
-    <div class="form-group">
-      <label for="inputJurusan">Jurusan</label>
-      <input type="text" class="form-control" id="inputJurusan" placeholder="Masukkan Jurusan">
-    </div>
-  <button type="submit" class="btn btn-primary">Add</button>
-</form>
-  </div>
-</div>
-        
-    </div>
+          <div class="form-row">
+          </div>
+          <div class="form-group">
+            <label for="inputNama">Nama</label>
+            <input type="text" class="form-control" id="inputNama" placeholder="Masukkan Nama">
+          </div>
+          <div class="form-group">
+            <label for="inputUsia">Usia</label>
+            <input type="text" class="form-control" id="inputUsia" placeholder="Masukkan Usia">
+          </div>
+          <div class="form-group">
+            <label for="inputJurusan">Jurusan</label>
+            <input type="text" class="form-control" id="inputJurusan" placeholder="Masukkan Jurusan">
+          </div>
+          <button type="submit" class="btn btn-primary">Add</button>
+        </form>
+      </div>
 </body>
 
 </html>
--- view/view_mahasiswa.php
+++ view/view_mahasiswa.php
@@ -2,54 +2,54 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
-        integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+    integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+  <title>Document</title>
 </head>
 
 <body>
-    
-    <div class="container p-3">
-      <div class="card text-center">
-  <div class="card-header">
-    <ul class="nav nav-tabs card-header-tabs">
-      <li class="nav-item">
-        <a class="nav-link active" href="view_mahasiswa.php">Mahasiswa</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link active" href="view_addmahasiswa.php">New Mahasiswa</a>
-      </li>
-    </ul>
-  </div>
-  <div class="card-body"></div>
-    <div class="container p-3">
+
+  <div class="container p-3">
+    <div class="card text-center">
+      <div class="card-header">
+        <ul class="nav nav-tabs card-header-tabs">
+          <li class="nav-item">
+            <a class="nav-link active" href="view_mahasiswa.php">Mahasiswa</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="view_addmahasiswa.php">New Mahasiswa</a>
+          </li>
+        </ul>
+      </div>
+      <div class="card-body"></div>
+      <div class="container p-3">
         <h1>Mahasiswa</h1>
-        
+
         <table class="table">
-            <thead class="thead-dark">
-                <tr>
-                    <th scope="col">No</th>
-                    <th scope="col">Nama</th>
-                    <th scope="col">Usia</th>
-                    <th scope="col">Jurusan</th>
-                    <th scope="col">Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                    <td>
-                        <button class="btn btn-primary">Edit</button>
-                        <button class="btn btn-danger">Delete</button>
-                </tr>
-            </tbody>
+          <thead class="thead-dark">
+            <tr>
+              <th scope="col">No</th>
+              <th scope="col">Nama</th>
+              <th scope="col">Usia</th>
+              <th scope="col">Jurusan</th>
+              <th scope="col">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+              <td>
+                <button class="btn btn-primary">Edit</button>
+                <button class="btn btn-danger">Delete</button>
+            </tr>
+          </tbody>
         </table>
-    </div>
+      </div>
     </div>
 </body>
 
