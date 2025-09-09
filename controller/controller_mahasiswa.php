478e7e7e910beed116db7e560f45b0a240b31010
Nicho <nicholasleroy83@gmail.com>
2025-09-09 18:12:34 +0700

    Update controller_mahasiswa.php

--- controller/controller_mahasiswa.php
+++ controller/controller_mahasiswa.php
@@ -2,15 +2,55 @@
 include("model_member.php");
 session_start();
 
-if (!isset($_SESSION['mahasiswalist'])) {
-    $_SESSION['mahasiswalist'] = array();
-}
 
-function createMahasiswa(){
+
+function createMahasiswa()
+{
     $mahasiswa = new model_mahasiswa();
     $mahasiswa->name = $_POST['inputName'];
     $mahasiswa->age = $_POST['inputAge'];
     $mahasiswa->major = $_POST['inputMajor'];
-    array_push($_SESSION['mahasiswalist'],$mahasiswa);
+    array_push($_SESSION['mahasiswalist'], $mahasiswa);
 }
-?>
+
+function updateMahasiswa($mahasiswaID)
+{
+    $mahasiswa = $_SESSION['mahasiswalist'][$mahasiswaID]; // ambil data dengan index tertentu
+    $mahasiswa->name = $_POST['inputName'];
+    $mahasiswa->age = $_POST['inputAge'];
+    $mahasiswa->major = $_POST['inputMajor'];
+}
+
+function getAllMahasiswa()
+{
+    return $_SESSION['mahasiswalist'];
+}
+
+function deleteMahasiswa($mahasiswaIndex)
+{
+    unset($_SESSION['mahasiswalist'][$mahasiswaIndex]);
+}
+
+function getMahasiswaWithID($mahasiswaID)
+{
+    return $_SESSION['mahasiswalist'][$mahasiswaID];
+}
+
+if (isset($_POST['button_register'])) {
+    createMahasiswa();
+    header("Location:view_mahasiswa.php");
+}
+
+if (!isset($_SESSION['mahasiswalist'])) {
+    $_SESSION['mahasiswalist'] = array();
+}
+
+if (isset($_GET['deleteID'])) {
+    deleteMahasiswa($_GET['deleteID']);
+    header("Location:view_mahasiswa.php");
+}
+
+if (isset($_POST['button_update'])) {
+    updateMahasiswa($_POST['input_id']);
+    header("Location:view_mahasiswa.php"); 
+}
